8fdb2a4bceb085f914311e4ebe71d5e8e473acfd
Jonathan Ross Hart <jonathan@jonathanrosshart.com>
2016-06-01 23:28:37 +0000

    Fixed my typo

--- mdtest.R
+++ mdtest.R
@@ -6,8 +6,5 @@ test$`hsa-let-7d`  # you can also pull out indiviual miRNAs using this format
 barplot(test[[1]]$depth)  # you can plot the depth using a barplot
 plot(test[[1]]$depth) # or the normal plotting system
 
-<<<<<<< HEAD
 PlotMrd(test, 'hsa-let-7d') # pretty plots
-=======
-PlotMrd(test, foo) # pretty plots
->>>>>>> 03fd82379d4681fefbbefeda7ed113bb14b9cc69
+
