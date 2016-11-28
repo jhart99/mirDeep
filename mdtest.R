e924c7a7b711a845eace970c99386689fcdd22ad
Jonathan Ross Hart <jonathan@jonathanrosshart.com>
2016-11-28 18:28:11 +0000

    Complete revamp of the parsing of mirdeep files.  Should handle both mirbase and discovery mirdeep files gracefully.  Added ability to parse mirbase records

--- mdtest.R
+++ mdtest.R
@@ -4,11 +4,23 @@ test2 <- ReadMrd("mirDeep2.mrd")
 test[[1]]  # to see the structure of the individual records
 test[['hsa-let-7d']]  # you can also pull out indiviual miRNAs using this format
 
-]
-barplot(test[[1]]$depth)  # you can plot the depth using a barplot
-plot(test[[1]]$depth) # or the normal plotting system
+barplot(test[[1]]$total.depth)  # you can plot the depth using a barplot
+plot(test[[1]]$total.depth) # or the normal plotting system
 
-PlotMrd(test, 1) # pretty plots
-PlotMrd(test, 15)
+PlotMrd(test[[1]]) # pretty plots
 
-CompareIDs(test, 1, 2)
+
+# plot a miRNA showing mismatches (possibly RNA editing)
+PlotMrdMM(test[['hsa-let-7d']])
+
+# compare two miRNAs
+CompareIDs(test[[1]], test[[2]])
+
+# Load miRNA data from miRbase (just save the page and load it)
+toot <- loadMirBaseHTML("miRNA Search Results.html")
+PlotMrd(toot)
+# or you can just copy and paste the URL
+library(RCurl)
+toot <- loadMirBaseHTML("http://www.mirbase.org/cgi-bin/get_read.pl?acc=MI0000269")
+PlotMrd(toot)
+
--- mirDeep.R
+++ mirDeep.R
@@ -28,45 +28,89 @@ ReadMrd <- function(file) {
   }
   
   #parse records
-  record.list <- lapply(record.list, function(x) {
-    out.list <- list()
-    # split on tabs first for the header part
-    record <- strsplit(x, "\\s+")
-
-    
-    out.list$total <- as.integer(record[[1]][4])
-    out.list$left <- as.integer(record[[2]][4])
-    out.list$right <- as.integer(record[[3]][4])
-    out.list$other <- as.integer(record[[4]][4])
-    if (record[[5]][1] == "exp") {
-      out.list$exp <- record[[5]][2]
-      out.list$pri_seq <- record[[6]][2]
-      out.list$pri_struct <- record[[7]][2]
-      begin <- 8
-    } else {
-      out.list$exp <- NA
-      out.list$pri_seq <- record[[5]][2]
-      out.list$pri_struct <- record[[6]][2]
-      begin <- 7
-    }
-    out.list$depth <- vector("integer", length(out.list$pri_seq))
-    for (line in record[-(1:begin)]) {
-      if (length(line) < 2 ) next
-      id <- strsplit(line[1], "\\_x")
-      count <- id[[1]][2]
-      # This next line parses the matches for stuff that isn't "." to give a
-      # vector of true/false values.  This is multiplied by the observed count
-      # to make an integer vector and then added to the existing totals
-      counts <- (strsplit(line[2], "")[[1]] != ".") * as.integer(count)
-      out.list$depth <- out.list$depth + counts
-    }
-    out.list
-  })
-  
+  record.list <- lapply(record.list, ParseMrdRecord)
   record.list
 }
-
-PlotMrd <- function(mrd, id, absolute=F) {
+ParseMrdRecord <- function(recordLines) {
+  # parses mrd output record into an mrd object
+  
+  out.list <- list()
+  
+  # split on whitespace
+  record <- strsplit(recordLines, "\\s+")
+  
+  # parse header
+  
+  if(record[[1]][1] == "total") { 
+    # mirbase style records
+    # here is where mirDeep gets dicey.  If there is an exact match of multiple
+    # miRNAs in a single primary sequence we will get a line for all of them. 
+    # This means there will be multiple and unpredictable lines of reads. 
+    i <- 1
+    while(length(record[[i]]) > 2) {
+      name <- record[[i]][1]
+      value <- record[[i]][4]
+      if(is.na(value)) value <- 0
+      out.list[[name]] <- value
+      i <- i + 1
+    }
+    begin <- i
+  } else {
+    # mirdeep discovery style records
+    out.list$score_total <- as.integer(record[[1]][3])
+    out.list$score_star <- as.integer(record[[2]][5])
+    out.list$score_mature <- as.integer(record[[3]][5])
+    out.list$score_mfe <- as.integer(record[[4]][4])
+    out.list$score_randfold <- as.integer(record[[5]][4])
+    out.list$score_seed <- as.integer(record[[6]][5])
+    out.list$miRNA_family <- record[[7]][5]
+    out.list$total <- as.integer(record[[8]][4])
+    out.list$mature <- as.integer(record[[9]][4])
+    out.list$loop <- as.integer(record[[10]][4])
+    out.list$star <- as.integer(record[[11]][4])
+    begin <- 12
+  }
+  # parse details. First find out the structure of the detail header
+  if (record[[begin]][1] == "exp") {
+    out.list$exp <- record[[begin]][2]
+    begin <- begin + 1
+  }
+  out.list$pri_seq <- record[[begin]][2]
+  out.list$pri_struct <- record[[begin+1]][2]
+  begin <- begin +2
+  
+  c(out.list, ParseMrdDetail(record[-(1:begin)]))
+  
+}
+ParseMrdDetail <- function(detailrecord) {
+  # filter out rows without any information first
+  detailrecord <- detailrecord[lapply(detailrecord, length) > 0]
+  # Pull out the depth values
+  depth <- lapply(detailrecord, function (x) {
+    (strsplit(x[1], "\\_x"))[[1]][2]
+  })
+  depth <- as.integer(unlist(depth))
+  
+  # Make a matrix of the alignments
+  alignments <- lapply(detailrecord, "[", 2)
+  if (is.null(unlist(lapply(alignments[!is.na(depth)], strsplit, "")))) {
+    return(list(depth=NULL, alignments=NULL))
+  } 
+  alignments <- t(matrix(unlist(lapply(alignments[!is.na(depth)], strsplit, "")), nrow=nchar(alignments[[1]])))
+  # remove rows that don't contain a depth as they are not useful
+  mirna.matches <- unlist(lapply(detailrecord[is.na(depth)], "[", 1))
+  depth <- depth[!is.na(depth)]
+  total.depth <- colSums((alignments != ".") * depth)
+  mismatch.depth <- colSums(apply(alignments, c(1,2), grepl, pattern='[A-Z]') * depth)
+  mismatched.reads <- sum(mismatch.depth)
+  list(depth=depth, 
+       alignments=alignments, 
+       total.depth = total.depth, 
+       mismatch.depth=mismatch.depth, 
+       mismatched.reads=mismatched.reads, 
+       mirna.matches=mirna.matches)
+}
+PlotMrd <- function(mrd, absolute=F) {
   # Plots an mrd record
   #
   # Args:
@@ -77,10 +121,9 @@ PlotMrd <- function(mrd, id, absolute=F) {
   #   a ggplot object
   
   library(ggplot2)
-  temp <- with(mrd[[id]], data.frame(exp=unlist(strsplit(exp, "")), 
-                                     pri_seq=unlist(strsplit(pri_seq, "")),
+  temp <- with(mrd, data.frame(pri_seq=unlist(strsplit(pri_seq, "")),
                                      pri_struct=unlist(strsplit(pri_struct, "")), 
-                                     depth=depth))
+                                     depth=total.depth))
   # if we are working in fractional depth, scale the depth
   temp$depth <- with(temp, 
                      if (absolute)
@@ -90,14 +133,44 @@ PlotMrd <- function(mrd, id, absolute=F) {
   )
   temp$loc <- 1:nrow(temp)
   
-  # There are some magic numbers to adjust the positioning of the graph so that we don't get the bases or folding overlapping with the graph.
-  plot <- ggplot(temp, aes(loc, depth)) + geom_line() + 
-    geom_point(aes(color=exp)) + 
-    geom_text(aes(label=pri_seq, color=exp), y=1.07*max(temp$depth), size=3) +
-    geom_text(aes(label=pri_struct, color=exp), y=1.04*max(temp$depth), size=3) +
+  # There are some magic numbers to adjust the positioning of the graph so that
+  # we don't get the bases or folding overlapping with the graph.
+  plot <- ggplot(temp, aes(loc, depth)) + geom_bar(stat="identity") + 
+    geom_text(aes(label=pri_seq), y=1.07*max(temp$depth), size=3) +
+    geom_text(aes(label=pri_struct), y=1.04*max(temp$depth), size=3) +
     theme(legend.position="none") + 
-    coord_cartesian(ylim=c(-0.025*max(temp$depth), 1.1*max(temp$depth))) +
-    ggtitle(id)
+    coord_cartesian(ylim=c(-0.025*max(temp$depth), 1.1*max(temp$depth)))
+  plot
+}
+
+PlotMrdMM <- function(mrd, absolute=F) {
+  # Plots an mrd record
+  #
+  # Args:
+  #   mrd: The mrd object
+  #   id: The id of the record you are interested in
+  #   absolute: Plot as the absolute depth or fractional?
+  # Returns:
+  #   a ggplot object
+  
+  library(ggplot2)
+  temp <- with(mrd, data.frame(depth=total.depth-mismatch.depth, MM=F))
+  temp <- rbind(temp, with(mrd, data.frame(depth=mismatch.depth, MM=T)))
+  temp$loc <- 1:length(mrd$total.depth)
+  
+  
+  # if we are working in fractional depth, scale the depth
+  temp$depth <- with(temp, 
+                     if (absolute)
+                       depth 
+                     else 
+                       depth/max(depth)
+  )
+  
+  # There are some magic numbers to adjust the positioning of the graph so that
+  # we don't get the bases or folding overlapping with the graph.
+  plot <- ggplot(temp, aes(loc, depth, fill=MM)) + geom_bar(stat="identity")
+    
   plot
 }
 
@@ -118,7 +191,7 @@ CompareMRD <- function(mrd1, mrd2, id) {
   ks.test(ecdf1,ecdf2)
 }
 
-CompareIDs <- function(mrd, id1, id2) {
+CompareIDs <- function(data1, data2) {
   # Compare two different mirDeep analyses
   #
   # Args:
@@ -127,10 +200,68 @@ CompareIDs <- function(mrd, id1, id2) {
   #   id: The id of the record you are interested in
   # Returns:
   #   a ks test object
-  ecdf1 <- cumsum(mrd[[id1]]$depth)
+  ecdf1 <- cumsum(data1$total.depth)
   ecdf1 <- ecdf1/max(ecdf1)
-  ecdf2 <- cumsum(mrd[[id2]]$depth)
+  ecdf2 <- cumsum(data2$total.depth)
   ecdf2 <- ecdf2/max(ecdf2)
-  
   ks.test(ecdf1,ecdf2)
+}
+
+loadMirBaseHTML <- function(html.in) {
+  out.list <- list()
+  library(XML)
+  doc <- htmlParse(html.in)
+  mirbase.table <- xpathSApply(doc, "//div[contains(@class, 'readSequence')]", xmlValue)
+  mirbase.table <- unlist(strsplit(mirbase.table, "\\n"))
+  mirbase.table <- strsplit(mirbase.table, "\\s+")
+  # filter out the blank rows
+  mirbase.table <- mirbase.table[lapply(mirbase.table, length) > 0]
+  # grab the names
+  out.list$mirna.matches <- mirbase.table[[1]][c(2,3)]
+  out.list$pri_seq <- mirbase.table[[length(mirbase.table)-1]]
+  out.list$pri_struct <- mirbase.table[[length(mirbase.table)]][1]
+  
+  mirbase.table <- mirbase.table[2:(length(mirbase.table)-2)]
+  
+  # depth vector
+  depth <- lapply(mirbase.table, "[", 2)
+  depth <- as.integer(unlist(depth))
+  
+  # Make a matrix of the alignments
+  alignments <- lapply(mirbase.table, "[", 1)
+  alignments <- t(matrix(unlist(lapply(alignments, strsplit, "")), nrow=nchar(alignments[[1]])))
+  total.depth <- colSums((alignments != ".") * depth)
+  
+  c(out.list, list(depth=depth, 
+       alignments=alignments, 
+       total.depth = total.depth))
+}
+
+ParseMirbaseDetail <- function(detailrecord) {
+  # filter out rows without any information first
+  detailrecord <- detailrecord[lapply(detailrecord, length) > 0]
+  # Pull out the depth values
+  depth <- lapply(detailrecord, function (x) {
+    (strsplit(x[1], "\\_x"))[[1]][2]
+  })
+  depth <- as.integer(unlist(depth))
+  
+  # Make a matrix of the alignments
+  alignments <- lapply(detailrecord, "[", 2)
+  if (is.null(unlist(lapply(alignments[!is.na(depth)], strsplit, "")))) {
+    return(list(depth=NULL, alignments=NULL))
+  } 
+  alignments <- t(matrix(unlist(lapply(alignments[!is.na(depth)], strsplit, "")), nrow=nchar(alignments[[1]])))
+  # remove rows that don't contain a depth as they are not useful
+  mirna.matches <- unlist(lapply(detailrecord[is.na(depth)], "[", 1))
+  depth <- depth[!is.na(depth)]
+  total.depth <- colSums((alignments != ".") * depth)
+  mismatch.depth <- colSums(apply(alignments, c(1,2), grepl, pattern='[A-Z]') * depth)
+  mismatched.reads <- sum(mismatch.depth)
+  list(depth=depth, 
+       alignments=alignments, 
+       total.depth = total.depth, 
+       mismatch.depth=mismatch.depth, 
+       mismatched.reads=mismatched.reads, 
+       mirna.matches=mirna.matches)
 }
